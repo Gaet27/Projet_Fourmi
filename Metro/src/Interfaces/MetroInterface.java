bea7a6461a0ea0198263325bf7cecc86dd602abb
Gaet27 <dion.gaetan@gmail.com>
2014-04-26 16:18:35 +0200

    Modifications des fichiers dans les packages (échange de fichiers dû à une erreur)

---
+++ Metro/src/Interfaces/MetroInterface.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+public interface MetroInterface {
+
+	//DECLARE FUNCTIONS
+	public void trouverProchaineStation();
+	
+	public void evoluer();
+
+	public void deposerPheromone();
+	
+}
